4d748b8d80768a4822707c3cddcb0b2347616374
Miriana Maranzano <mirianamaranzano2@gmail.com>
2021-12-07 17:43:54 +0100

    feature:CV ex done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
+import foto from './Mais.jpeg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header className="CV">
+        <div className="top">
+        <img src={foto} className="foto" alt="foto" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <h3> Miriana Maranzano </h3>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        </div>
+        <div className="description">
+
+          <h4> Description </h4>
+          <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </p>
+        </div>
+
+        <div className="container">
+
+          <div> <h4>Esperienze Lavorative</h4>
+            <p>
+              Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            </p>  </div>
+
+
+          <div> <h4> Languages </h4>
+            <p> Italian, English and Spanish</p> </div>
+
+          <div> <h4> Hobby </h4>
+            <p> Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p> </div>
+
+        </div>
+
+        <div className="Mycontacts">
+          <form>
+            <h4> Contact me </h4>
+            <label for="contact">Object</label>
+            <textarea id="obj"> </textarea>
+            <label for="contact">Message</label>
+            <textarea id="msg"> </textarea>
+          </form>
+        </div>
+
+        <div className="footer">
+          <p>miriana.maranzano@virgilio.it</p>
+        </div>
       </header>
     </div>
+
   );
 }
 
